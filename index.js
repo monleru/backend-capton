23fe3d6e5967b05ed33da96a80cafa9904d97129
unknown <kucherenko.alexandr1998@gmail.com>
2022-08-30 15:30:49 +0300

    cors

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
 const router = require('./router.js')
 
 const PORT = process.env.PORT || 5000;
 const DB_URL = "mongodb+srv://nikko:MoKm3lxo6hjiSpEN@cluster0.8kzaexv.mongodb.net/?retryWrites=true&w=majority"
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
 app.post('/', )
 async function startApp() {
     try {
